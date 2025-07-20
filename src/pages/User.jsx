72f4b0760816f88569e75bff7396aaad75129733
Samuel <samg.martinez2@gmail.com>
2025-07-20 00:00:59 -0300

    Reinicio del proyecto

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,6 +5,9 @@ const userPage = () => {
     <div>
       <h1>User Page</h1>
       <p>This is the user page content.</p>
+
+      <h1 class="text-3xl font-bold underline bg-red-300">    Hello world! con tailwing  </h1>
+
     </div>
   );
 }
